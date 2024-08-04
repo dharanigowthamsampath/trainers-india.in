9d196793050cf62ead1a24d412433aa55ca321e2
Dharani Gowtham <dharanigowthamsampath@gmail.com>
2024-08-04 19:20:16 +0530

    pad

--- app/(users)/dashboard/page.tsx
+++ app/(users)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const Dashboard = (props: Props) => {
-  return <div>Helo</div>;
+  return <div className="p-4">Helo</div>;
 };
 
 export default Dashboard;
