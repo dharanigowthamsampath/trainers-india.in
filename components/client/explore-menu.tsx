d4a1a66b712c305ff987d20297b59652088c8589
Dharani Gowtham <dharanigowthamsampath@gmail.com>
2024-08-10 11:18:13 +0530

    Added Loader to the Explore

--- components/client/explore-menu.tsx
+++ components/client/explore-menu.tsx
@@ -1,12 +1,11 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
 import JobDescription from "./job-description";
 import { getJobs } from "@/server/GetJobPosts";
 import JobList from "../ui/job-list";
+import Loader from "../loader";
 
 type Props = {};
-
 type Job = {
   id: string;
   title: string;
@@ -21,14 +20,22 @@ type Job = {
 const ExploreMenuComponent: React.FC<Props> = () => {
   const [jobs, setJobs] = useState<Job[]>([]);
   const [selectedJob, setSelectedJob] = useState<Job | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const result = await getJobs();
-      if (result.success && result.jobs) {
-        setJobs(result.jobs || []);
-      } else {
-        console.error(result.error || "Failed to fetch jobs");
+      setIsLoading(true);
+      try {
+        const result = await getJobs();
+        if (result.success && result.jobs) {
+          setJobs(result.jobs || []);
+        } else {
+          console.error(result.error || "Failed to fetch jobs");
+        }
+      } catch (error) {
+        console.error("Error fetching jobs:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchJobs();
@@ -37,11 +44,17 @@ const ExploreMenuComponent: React.FC<Props> = () => {
   return (
     <div className="w-full h-full flex flex-col md:flex-row">
       <div className="w-full md:w-1/3 bg-white flex-shrink-0 mb-2 md:mb-0 md:mr-1 overflow-y-auto h-[50vh] md:h-full">
-        <JobList
-          jobs={jobs}
-          onSelectJob={setSelectedJob}
-          selectedJob={selectedJob}
-        />
+        {isLoading ? (
+          <div className="flex justify-center items-center h-full">
+            <Loader size="small" />
+          </div>
+        ) : (
+          <JobList
+            jobs={jobs}
+            onSelectJob={setSelectedJob}
+            selectedJob={selectedJob}
+          />
+        )}
       </div>
       <div className="w-full md:w-2/3 bg-white flex-grow overflow-y-auto h-[50vh] md:h-full">
         <JobDescription job={selectedJob} />
--- components/loader.tsx
+++ components/loader.tsx
@@ -1,26 +1,21 @@
 "use client";
-
 import React from "react";
 
 type LoaderProps = {
   size?: "small" | "medium" | "large";
-  color?: string;
 };
 
-const Loader: React.FC<LoaderProps> = ({
-  size = "medium",
-  color = "border-gray-900",
-}) => {
+const Loader: React.FC<LoaderProps> = ({ size = "medium" }) => {
   const sizeClasses = {
     small: "h-6 w-6 border-2",
-    medium: "h-12 w-12 border-2",
+    medium: "h-12 w-12 border-4",
     large: "h-16 w-16 border-4",
   };
 
   return (
     <div className="flex justify-center items-center">
       <div
-        className={`animate-spin rounded-full ${sizeClasses[size]} border-t-${color} border-r-transparent border-b-${color} border-l-transparent`}
+        className={`animate-spin rounded-full ${sizeClasses[size]} border-blue-500 border-t-transparent`}
       ></div>
     </div>
   );
