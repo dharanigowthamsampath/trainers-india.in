e179aeef26ad3bf0e27e473a8317eb1de6a586ff
Dharani Gowtham <dharanigowthamsampath@gmail.com>
2024-08-11 14:30:55 +0530

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,50 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex items-center justify-center">
-      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-5">
-        <div className="px-4 py-2 bg-blue-600 text-white w-40 flex justify-center rounded-lg">
-          <Link
-            className="w-full h-full flex items-center justify-center text-base text-white"
-            href={"/job"}
-          >
-            Add Job
+    // <main className="w-full h-screen flex items-center justify-center bg-gray-100">
+    //   <div className="bg-white shadow-lg p-8 w-96">
+    //     <h1 className="text-3xl font-bold mb-6 text-center">Trainers India</h1>
+    //     <div className="space-y-4 mb-6">
+    //       <Link
+    //         href="/explore-job"
+    //         className="bg-blue-500 text-white px-8 py-3 w-full block text-center hover:bg-blue-600 transition duration-300"
+    //       >
+    //         Explore Job
+    //       </Link>
+    //       <Link
+    //         href="/add-job"
+    //         className="bg-blue-500 text-white px-8 py-3 w-full block text-center hover:bg-blue-600 transition duration-300"
+    //       >
+    //         Add Job
+    //       </Link>
+    //     </div>
+    //     <p className="text-gray-600 italic text-center text-sm">
+    //       Built by a trainer for the trainers
+    //     </p>
+    //   </div>
+    // </main>
+    <main className="w-full h-full flex justify-center items-center">
+      <div className=" shadow-lg p-10">
+        <div className="pb-4">
+          <div className="font-bold text-2xl text-blue-800 text-center">TE</div>
+          <div className="text-center">Trainers India</div>
+        </div>
+
+        <div className="pb-5 flex justify-center">
+          <Link href={"/job"} className="px-10 border py-2 border-blue-600">
+            Explore Job
           </Link>
         </div>
-        <div className="px-4 py-2 bg-blue-600 text-white w-40 flex justify-center rounded-lg">
+        <div className=" text-xs font-medium text-gray-400">
+          Build by a{" "}
           <Link
-            className="w-full h-full flex items-center justify-center text-base text-white"
-            href={"/job"}
+            href={"https://www.linkedin.com/in/dharani-gowtham/"}
+            target="_blank"
           >
-            View Job
-          </Link>
+            Trainer
+          </Link>{" "}
+          for the Trainers
         </div>
-        <Link
-          href={"/job"}
-          className="px-10 py-2 bg-blue-600 rounded-lg text-white"
-        >
-          {" "}
-          Add Job
-        </Link>
       </div>
     </main>
   );
