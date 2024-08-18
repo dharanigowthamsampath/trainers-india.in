e4375ad996cc064f41ed507502ea326c8e964770
Dharani Gowtham <dharanigowthamsampath@gmail.com>
2024-08-18 12:56:32 +0530

    Added Web analytics to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin", "latin-ext"],
@@ -46,6 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Analytics />
       <html lang="en">
         <body className={poppins.className}>{children}</body>
       </html>
